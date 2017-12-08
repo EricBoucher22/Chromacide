45dce6aa0968414f8ad55d92adb69a2a4d0d6335
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-08 16:05:30 +0100

    update

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,29 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField] private PartyController _partyController;
-    private CharacterComponent _controlledCharacter;
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private PartyController _partyController;
+	private CharacterComponent _controlledCharacter;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
 		
-    }
+	}
 	
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            _partyController.Next();
-        }
-    }
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Backspace)) {
+			_partyController.Next ();
+		}
+	}
 
-    public void PlayCharacter(CharacterComponent character)
-    {
-        _controlledCharacter = character;
-    }
+	public void PlayCharacter (CharacterComponent character) {
+		_controlledCharacter = character;
+	}
+
 
 }
