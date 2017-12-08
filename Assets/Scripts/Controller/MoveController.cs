e40f1b50e7819ca5ad5b4da7bd79851fd4e0ea1c
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-08 16:49:35 +0100

    add move controller

---
+++ Assets/Scripts/Controller/MoveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+	private Dictionary<GameObject, List<Vector3>> _moveToQueue;
+
+	// Use this for initialization
+	void Start () {
+		_moveToQueue = new Dictionary<GameObject, List<Vector3>> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (KeyValuePair<GameObject, List<Vector3>> moveTo in _moveToQueue) {
+			
+		}
+	}
+
+	public void MoveTo (GameObject toMove, List<Vector3> positions) {
+		if (_moveToQueue.ContainsKey (toMove)) {
+			_moveToQueue [toMove].AddRange (positions);
+		} else {
+			_moveToQueue.Add (toMove, positions);
+		}
+	}
+}
