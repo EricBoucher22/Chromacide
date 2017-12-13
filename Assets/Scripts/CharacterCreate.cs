5199c34219faa2b539fb8f59fbfd72cb7fb52e33
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-13 16:19:04 +0100

    correct stuff

--- Assets/Scripts/CharacterCreate.cs
+++ Assets/Scripts/CharacterCreate.cs
@@ -3,32 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class CharacterCreate : MonoBehaviour
-{
+public class CharacterCreate : MonoBehaviour {
 	[SerializeField] private Character enemy = null;
 	[SerializeField] private Character[] player = null;
 
 	public static List<Character> Characters = new List<Character> ();
 
-	void Awake()
-	{
+	void Awake () {
 	}
 
 	// Use this for initialization
-	void Start ()
-	{
-		foreach (Character cha in player)
-		{
+	void Start () {
+		foreach (Character cha in player) {
 			cha.CreateCharacter (transform);
 			Characters.Add (cha);
 		}
 
 		Map.ActivateColor (Characters [0].AffinityColor);
+
 	}
 
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update () {
 
 	}
 }
