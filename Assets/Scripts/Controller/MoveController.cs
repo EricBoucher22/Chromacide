85dc9c82791382fe0bff7e6c1f4a3bb83378a3d9
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-13 13:46:54 +0100

    manage movement

--- Assets/Scripts/Controller/MoveController.cs
+++ Assets/Scripts/Controller/MoveController.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveController : MonoBehaviour {
-
-	private Dictionary<GameObject, List<Vector3>> _moveToQueue;
+	[SerializeField] private float _nominalSpeed;
+	private Dictionary<NavMeshAgent, List<Vector3>> _moveToQueue;
 
 	// Use this for initialization
 	void Start () {
 		_moveToQueue = new Dictionary<GameObject, List<Vector3>> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		foreach (KeyValuePair<GameObject, List<Vector3>> moveTo in _moveToQueue) {
-			
+
+	void FixedUpdate () {
+		List<NavMeshAgent> agents_to_remove = new List<NavMeshAgent> ();
+		foreach (KeyValuePair<NavMeshAgent, List<Vector3>> moveTo in _moveToQueue) {
+			NavMeshAgent agent = moveTo.Key;
+			if (!agent.pathPending && !agent.hasPath) {
+				agent.destination = moveTo.Value [0];
+				moveTo.Value.RemoveAt (0);
+				if (moveTo.Value.Count == 0) {
+					agents_to_remove.Add (agent);
+				}
+			}
+		}
+
+		foreach (NavMeshAgent agent in agents_to_remove) {
+			_moveToQueue.Remove (agent);
 		}
 	}
 
--- Assets/Scripts/Map/BlockCharacterMovement.cs
+++ Assets/Scripts/Map/BlockCharacterMovement.cs
@@ -3,50 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class BlockCharacterMovement : MonoBehaviour
-{
+public class BlockCharacterMovement : MonoBehaviour {
 	Stack path;
 	List<Vector3> pathPositions;
 
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		path = new Stack ();
 		pathPositions = new List<Vector3> ();
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update () {
 		
 	}
 
-	void OnMouseEnter()
-	{
+	void OnMouseEnter () {
 		Character active = CharacterCreate.Characters [0];
-		path = AStarFunctions.AStar(MapCreate.PositionReference - MapCreate.PositionReference, active.Instance.transform.position, transform.parent.position - MapCreate.PositionReference, active.AffinityColor, active.MaxSquares);
+		path = AStarFunctions.AStar (MapCreate.PositionReference - MapCreate.PositionReference, active.Instance.transform.position, transform.parent.position - MapCreate.PositionReference, active.AffinityColor, active.MaxSquares);
 
-		foreach(Vector2 arrayPos in path)
-		{
-			Map.MapGameObjects [(int) arrayPos.x, (int) arrayPos.y].transform.GetChild(0).GetComponent<BlockPropertyChanger> ().HighlightMaterial ();
+		foreach (Vector2 arrayPos in path) {
+			Map.MapGameObjects [(int)arrayPos.x, (int)arrayPos.y].transform.GetChild (0).GetComponent<BlockPropertyChanger> ().HighlightMaterial ();
 		}
 	}
 
-	void OnMouseExit()
-	{
-		foreach(Vector2 arrayPos in path)
-		{
-			Map.MapGameObjects [(int) arrayPos.x, (int) arrayPos.y].transform.GetChild(0).GetComponent<BlockPropertyChanger> ().BaseMaterial ();
+	void OnMouseExit () {
+		foreach (Vector2 arrayPos in path) {
+			Map.MapGameObjects [(int)arrayPos.x, (int)arrayPos.y].transform.GetChild (0).GetComponent<BlockPropertyChanger> ().BaseMaterial ();
 		}
 	}
 
-	void OnMouseDown()
-	{
+	void OnMouseDown () {
 		pathPositions.Clear ();
 		// make the the position list
-		foreach(Vector2 arrayPosition in path)
-		{
-			pathPositions.Add (new Vector3(arrayPosition.y, 0, -arrayPosition.x) + MapCreate.PositionReference);
+		foreach (Vector2 arrayPosition in path) {
+			pathPositions.Add (new Vector3 (arrayPosition.y, 0, -arrayPosition.x) + MapCreate.PositionReference);
 		}
 		// call the function of movement
 
