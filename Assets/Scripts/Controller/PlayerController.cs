f7c4f98fc9a2c82350718214a6caa89d749dcd6e
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-13 13:46:54 +0100

    manage movement

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	[SerializeField] private PartyController _partyController;
 	private CharacterComponent _controlledCharacter;
+	private bool _inputFrozen;
 
 	// Use this for initialization
 	void Start () {
-		
+		_inputFrozen = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// while perfoming action, the input is frozen.
+
 		if (Input.GetKeyDown (KeyCode.Backspace)) {
 			_partyController.Next ();
 		}
