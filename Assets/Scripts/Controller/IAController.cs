d4df6d9470ec277569c81a7c4508ee4feb6346e0
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-12-14 16:44:22 +0100

    add ia controller

---
+++ Assets/Scripts/Controller/IAController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAController : MonoBehaviour, ICharacterController {
+
+	private CharacterComponent _controlledCharacter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayCharacter (CharacterComponent character) {
+		_controlledCharacter = character;
+	}
+}
